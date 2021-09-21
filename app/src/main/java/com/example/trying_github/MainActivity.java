d340bf51320eba31367784bbbaadfb2704ccf9e9
Calvin <bintot914@gmail.com>
2021-09-21 01:25:49 -0400

    enabled view binding

--- app/src/main/java/com/example/trying_github/MainActivity.java
+++ app/src/main/java/com/example/trying_github/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.trying_github.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
